026127c413aeacc921fd248a85bd6c7c6dea384b
Alexandr <IamWebDev@gmail.com>
2026-03-25 13:28:56 -0400

    Update start script to include dependency installation before migrations

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+echo "Changing to build directory..."
+cd .medusa/server || exit
+
+echo "Installing production dependencies..."
+pnpm install
+
 echo "Running database migrations..."
 pnpm medusa db:migrate
-
-cd .medusa/server || exit
 
 # Seed only on first run
 SEED_MARKER=".seeded"
